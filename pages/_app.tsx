25882034a35b9a50291cceaad3269e5d28492ded
Nathaly Riaño <natharb09@gmail.com>
2022-01-10 10:32:59 -0500

    setting up eslint and prettier rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '@foundations'
 import type { AppProps } from 'next/app'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { theme } from '@foundations'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ChakraProvider theme={theme}><Component {...pageProps} /></ChakraProvider>
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
