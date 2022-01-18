914d7078e8fc9a57aea95ebacaa175d470c6c034
Nathaly Riaño <natharb09@gmail.com>
2022-01-18 18:20:29 -0500

    adding coin details

--- components/04_templates/home/Home.tsx
+++ components/04_templates/home/Home.tsx
@@ -1,4 +1,13 @@
-import { Container, Skeleton } from '@chakra-ui/react'
+import {
+  Box,
+  Heading,
+  Stat,
+  StatArrow,
+  StatGroup,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+} from '@chakra-ui/react'
 import React from 'react'
 
 import { ICoinData } from 'features/coins'
@@ -10,14 +19,60 @@ interface Props {
 
 export const Home: React.FC<Props> = ({ data }) => {
   return (
-    <Container as="main" color="white" h="full" pos="fixed" w="full">
+    <Box as="main" color="white" h="full" p="4" pos="fixed" w="70%">
       {data && (
         <>
-          <h1>{data.name}</h1>
-          <h2>{data.rank}</h2>
-          <h3>{data.price}</h3>
+          <StatGroup flexDirection="column" gap="4">
+            <Stat>
+              <StatLabel>Rank</StatLabel>
+              <StatNumber>{data.rank}</StatNumber>
+            </Stat>
+
+            <Stat>
+              <StatLabel>Name</StatLabel>
+              <StatNumber>{data.name}</StatNumber>
+            </Stat>
+
+            <Stat>
+              <StatLabel>Price</StatLabel>
+              <StatNumber>{data.price}</StatNumber>
+              <StatHelpText>
+                <StatArrow type={data.change >= 0 ? 'increase' : 'decrease'} />
+                {data.change}
+              </StatHelpText>
+            </Stat>
+
+            <Stat>
+              <StatLabel>Market Cap</StatLabel>
+              <StatNumber>{data.marketCap}</StatNumber>
+            </Stat>
+
+            <Stat>
+              <StatLabel>24H Volume</StatLabel>
+              <StatNumber>{data.volume}</StatNumber>
+            </Stat>
+
+            <Stat>
+              <StatLabel>Circulating Supply</StatLabel>
+              <StatNumber>{data.supply}</StatNumber>
+            </Stat>
+          </StatGroup>
+
+          <Heading
+            as="h1"
+            bottom="0"
+            filter="blur(6px)"
+            fontSize="20em"
+            opacity="0.15"
+            pos="absolute"
+            right="0"
+            size="4xl"
+            textTransform="uppercase"
+          >
+            {data.symbol}
+          </Heading>
         </>
       )}
-    </Container>
+    </Box>
   )
 }
